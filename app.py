bb660161fefc6838ba261d79e37f84562a565838
AlvaroBadiaAlcaraz <alvaro.badia@gmail.com>
2023-12-06 13:03:09 +0100

    Commit inicial

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+@app.route("/pizza", methods = ['POST'])
+
+def get_data():
+    nombre = request.form.get("p1")
+    apellido = request.form.get("p2")
+
+    print(nombre)
+    print(apellido)
+
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write(nombre + " " + apellido + "\n")
+        file.close()
+
+    return redirect("http://localhost/PIZZA FULL STACK RELEASE/solicita_pedido.html", code=302)
