70f7d11274372b361fc968a12c07775e5654a0d2
AlvaroBadiaAlcaraz <alvaro.badia@gmail.com>
2023-12-12 18:08:55 +0100

    contenido persistencia y test

---
+++ persistencia.py
@@ -0,0 +1,11 @@
+"""
+Api Persistencia
+"""
+
+def guardar_pedido(nombre, apellidos):
+    """
+    Guarda un pedido
+    """
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write(nombre + "" + apellidos)
+        file.close()
